02c492da46fdff74d8375680b6ce02b027cbcf55
mob_ <100270525+nitinkrishnan@users.noreply.github.com>
2023-08-01 08:16:51 +0530

    Add files via upload

---
+++ Python for DS/Basics/User Inputs/input.py
@@ -0,0 +1,8 @@
+
+#Python allows for user input.
+#That means we are able to ask the user for input.
+
+name = input("Enter your name: ")
+print("Hello , are you " +name+ "?")
+
+
---
+++ Python for DS/Basics/loops/for_loop.py
@@ -0,0 +1,12 @@
+
+# for loop
+cars = ["toyota" , "audi" , "bmw"]
+for x in cars:
+   print(x)
+   
+
+car = "toyota"
+for x in car:
+  print(x)
+   
+   
