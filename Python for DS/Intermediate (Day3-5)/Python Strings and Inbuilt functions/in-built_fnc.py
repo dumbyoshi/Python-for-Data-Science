97d79a860bedbfa2067a8dfdf7a71801017b06b5
mob_ <100270525+nitinkrishnan@users.noreply.github.com>
2023-09-02 19:03:30 +0530

    Add files via upload

---
+++ Python for DS/Intermediate (Day3-5)/Python Strings and Inbuilt functions/in-built_fnc.py
@@ -0,0 +1,19 @@
+#Python Strings and its inbuilt functions:
+#capitalize() function
+name = 'nithin'
+print(name.capitalize())
+#upper() function
+print(name.upper())
+#lower() function
+print(name.lower())
+#title() function
+print(name.title())
+#swapcase() function
+print(name.swapcase())
+#rpartition() function
+print(name.rpartition('i'))
+
+#https://www.pythonmorsels.com/string-methods/
+#https://www.geeksforgeeks.org/python-string-methods/
+#https://www.tutorialsteacher.com/python/string-methods
+#https://www.studytonight.com/python/string-functions
